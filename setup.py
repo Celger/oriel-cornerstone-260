1fcdc4d4d63a70d6b16b0f545cf450960db44461
bicarlsen <carlsen.bri@gmail.com>
2020-05-06 10:17:30 +0200

    Changed license.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha"
     ],
